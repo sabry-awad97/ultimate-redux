13dd29966853db114a90bd82effe7f18d0c55d15
Sabry Awad <dr.sabry1997@gmail.com>
2022-09-07 22:50:53 +0200

    converting if to switch

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -4,25 +4,24 @@ import { Task } from '../types/Task';
 // The real point of reducers is to handle actions.
 // Reducers are pure functions that update state in response to actions.
 export default (state = { tasks: [] as Task[] }, action: Action) => {
-  if (action.type === ActionTypes.CREATE_TASK_SUCCEEDED) {
-    return { tasks: state.tasks.concat(action.payload.task) };
-  }
+  switch (action.type) {
+    case ActionTypes.CREATE_TASK_SUCCEEDED:
+      return { tasks: state.tasks.concat(action.payload.task) };
 
-  if (action.type === ActionTypes.EDIT_TASK) {
-    const { payload } = action;
-    return {
-      tasks: state.tasks.map(task =>
-        task.id === payload.id ? { ...task, ...payload.params } : task
-      ),
-    };
-  }
+    case ActionTypes.EDIT_TASK:
+      const { payload } = action;
+      return {
+        tasks: state.tasks.map(task =>
+          task.id === payload.id ? { ...task, ...payload.params } : task
+        ),
+      };
 
-  // The reducer listens for the server action
-  if (action.type === ActionTypes.FETCH_TASKS_SUCCEEDED) {
-    return {
-      tasks: action.payload.tasks,
-    };
-  }
+    case ActionTypes.FETCH_TASKS_SUCCEEDED:
+      return {
+        tasks: action.payload.tasks,
+      };
 
-  return state;
+    default:
+      return state;
+  }
 };
